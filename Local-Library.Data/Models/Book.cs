db04dd1c39a4cf09c87cf1425ede4c47bf121cd2
Robert Siciliano <Robert.Siciliano22@gmail.com>
2024-06-29 21:40:12 -0400

    Book model

---
+++ Local-Library.Data/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Local_Library.Data.Models
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string CoverImage { get; set; }
+        public string Publisher { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public Category Category { get; set; }
+        public Guid ISBN { get; set; }
+        public int PageCount { get; set; }
+    }
+}
---
+++ Local-Library.Data/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Local_Library.Data.Models
+{
+    public enum Category
+    {
+        NonFiction,
+        Fiction,
+        Autobiography,
+        Biography,
+        Children,
+        SelfHelp,
+        Horror
+    }
+}
